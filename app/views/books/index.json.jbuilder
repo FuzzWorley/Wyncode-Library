96a06a9c9b4157e36752d06c4909d5a395fcb007
Amy Dusek <amlydu@umich.edu>
2015-05-06 13:19:34 -0400

    Changed the quantity and checkout length; ran migration to remove_columns from the database; added sign-in form

--- app/views/books/index.json.jbuilder
+++ app/views/books/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@books) do |book|
-  json.extract! book, :id, :title, :author, :quantity, :checkout_length
+  json.extract! book, :id, :title, :author
   json.url book_url(book, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506024313) do
+ActiveRecord::Schema.define(version: 20150506171242) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
-    t.integer  "quantity"
-    t.integer  "checkout_length"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "checkouts", force: :cascade do |t|
