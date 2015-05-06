9070ad61cc3dc5facbf57bc1ce8456eacc98f2e3
Steven Worley <bitcopia@gmail.com>
2015-05-06 17:09:40 -0400

    trying to create checkouts and link

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,18 @@
+class CheckoutsController < ApplicationController
+	# before_action :set_checkout, only: [:show, :edit, :update, :destroy]
+
+  def create
+  	@checkout = current_user.checkouts.create(checkout_params)
+
+  	redirect_to root_path
+  end 
+
+  private
+    # def set_checkout
+    #   @checkout = Checkout.find(params[:id])
+    # end
+
+  def checkout_params
+  	params.require(:checkout).permit(:book_id, :user_id)
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ActiveRecord::Base
 	has_many :checkouts
 	has_many :users, through: :checkouts
 
+	#has_one :user???
+
 	def checkedout?
 		Checkout.exists?(book_id: id)
 	end
